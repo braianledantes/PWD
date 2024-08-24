fbbbeb8d6a641b76a3fec2a59a80b216583ed39a
ClaraMelaniePelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2024-08-24 16:57:12 -0300

    Agregue menu de ejercicios

---
+++ menu.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PWD 2024 </title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="menu.css">
+</head>
+    <header>
+        <h1>Menú de Trabajos Practicos</h1>
+        <h4>Grupo #2</h4>
+    </header>
+    <section>
+    <div class="accordion" id="accordionExample">
+<!--Trabajo Practico No 1-->
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Trabajo Práctico #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <ul class="list-group">
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 1</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 2</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 3</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 4</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 5</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 6</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 7</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 8</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+<!--Trabajo Practico No 2-->
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+      Trabajo Práctico #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <ul class="list-group">
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 1</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 2</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 3</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 4</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+<!--Trabajo Practico No 3-->
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      Trabajo Práctico #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <ul class="list-group">
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 1</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 2</a></li>
+            <li class="list-group-item"><a class="dropdown-item" href="TP3/vistas/ejercicio1.php">Ejercicio 3</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+    </section>
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
