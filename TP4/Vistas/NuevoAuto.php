b0c17fa0e2b411b2bbd8a5d7395fdcb8674ccf3a
Braian Ledantes <braianledantes@gmail.com>
2024-09-11 16:11:06 -0300

    bootstrap para nuevoauto.php

--- TP4/Vistas/NuevoAuto.php
+++ TP4/Vistas/NuevoAuto.php
@@ -5,30 +5,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nuevo Auto | TP4</title>
+    <?php include_once './estructura/bootstrap.php'; ?>
 </head>
 
 <body>
 
-    <h1>Crear Nuevo Auto</h1>
+    <?php include_once './estructura/header.php'; ?>
 
-    <form action="accionNuevoAuto.php" method="post">
-        <label for="patente">Patente:</label>
-        <input type="text" name="patente" id="patente" required>
-        <br>
-        <label for="marca">Marca:</label>
-        <input type="text" name="marca" id="marca" required>
-        <br>
-        <label for="modelo">Modelo:</label>
-        <input type="text" name="modelo" id="modelo" required>
-        <br>
-        <label for="dniDuenio">DNI Dueño:</label>
-        <input type="text" name="dniDuenio" id="dniDuenio" required>
-        <br>
-        <button type="submit">Crear</button>
-    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center my-4">Crear Nuevo Auto</h2>
+            </div>
 
-    <a href="listaAutos.php">Volver</a>
+            <div class="col-12 d-flex justify-content-center">
+                <form action="accionNuevoAuto.php" method="post" style="width: 600px; max-width: 600px;" class="border p-3 rounded">
+                    <div class="row mb-3">
+                        <label class="form-label col-sm-2 col-form-label" for="patente">Patente:</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="patente" id="patente" required>
+                        </div>
+                    </div>
 
+                    <div class="row mb-3">
+                        <label class="form-label col-sm-2 col-form-label" for="marca">Marca:</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="marca" id="marca" required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="form-label col-sm-2 col-form-label" for="modelo">Modelo:</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="modelo" id="modelo" required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="form-label col-sm-2 col-form-label" for="dniDuenio">DNI Dueño:</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="dniDuenio" id="dniDuenio" required>
+                        </div>
+                    </div>
+
+                    <button class="btn btn-primary" type="submit">Crear</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
