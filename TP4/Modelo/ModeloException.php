be24b676d43a11baa06996545a671dfb978fc338
Braian Ledantes <braianledantes@gmail.com>
2024-09-17 20:34:04 -0300

    correccion error de parametro de mas

--- TP4/Modelo/ModeloException.php
+++ TP4/Modelo/ModeloException.php
@@ -2,7 +2,7 @@
 
 class ModeloException extends RuntimeException
 {
-    public function __construct($message, $codigo = 0, Exception $previous  = null)
+    public function __construct($message, Exception $previous  = null)
     {
         parent::__construct($message, 0, $previous);
     }
