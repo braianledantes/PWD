e2281d21856a57f192f98be7f120bd11dac5090a
Braian Ledantes <braianledantes@gmail.com>
2024-10-15 09:26:14 -0300

    vistar iniciales

---
+++ TP-Librerias/vistas/index.php
@@ -0,0 +1,21 @@
+<?php
+$title = "TP - Librerias";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+
+<body>
+    <h1><?= $title ?></h1>
+
+    <a href="../tests/phpdotenv.php">Test de libreria phpdotenv</a>
+    <a href="../tests/UnitConverter.php">Test de libreria de UnitConverter</a>
+</body>
+
+</html>
