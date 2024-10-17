17a7c92265d0503f24a27ef9f99b65cac75d873c
ClaraMelaniePelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2024-10-17 17:13:25 -0300

    agregue estilos

--- TP-Librerias/vistas/converter.php
+++ TP-Librerias/vistas/converter.php
@@ -14,33 +14,17 @@ include_once '../configuracion.php';
 <body>
     <?php include_once './estructura/header.php'; ?>
     
-    <div class="container">
+    <div class="contenedor mx-auto" style="margin: 40px; width: 80%; max-width: 600px;">
     <h1>Conversor de Unidades</h1>
     <form action= "./accion/accionConverter.php" method="POST">
-    <label for="value">Valor:</label>
-    <input type="number" name="value" required>
-    
-    <label for="fromUnit">Desde:</label>
 
-    <select name="fromUnit">
-        <option value="cm">Centímetros</option>
-        <option value="dm">Deccimetros</option>
-        <option value="m">Metros</option>
-        <option value="ft">Pies</option>
-        <option value="h">Manos</option>
-        <option value="in">Pulgadas</option>
-        <option value="km">Kilometros</option>
-        <option value="um">Micrometros</option>
-        <option value="mi">Millas</option>
-        <option value="mm">Milimetros</option>
-        <option value="nm">Nanometros</option>
-        <option value="pc">Picometros</option>
-        <option value="yd">Yardas</option>
-        
-    </select>
-    
-    <label for="toUnit">A:</label>
-    <select name="toUnit">
+    <div class="form-floating mb-3">
+        <input type="number" name="value" class="form-control" id="floatingInput" placeholder="cualquier numero" required>
+        <label for="value">Valor Numerico</label>
+    </div>
+   
+    <label for="fromUnit" class="mb-3">Desde:</label>
+    <select name="fromUnit" class="form-select mb-3">
     <option value="cm">Centímetros</option>
         <option value="dm">Deccimetros</option>
         <option value="m">Metros</option>
@@ -54,10 +38,27 @@ include_once '../configuracion.php';
         <option value="nm">Nanometros</option>
         <option value="pc">Picometros</option>
         <option value="yd">Yardas</option>
+    </select> 
+    
+    <label for="toUnit" class="mb-3">Convertir a:</label>
+    <select name="toUnit" class="form-select">
+    <option value="in">Pulgadas</option>
+    <option value="cm">Centímetros</option>
+        <option value="dm">Deccimetros</option>
+        <option value="m">Metros</option>
+        <option value="ft">Pies</option>
+        <option value="h">Manos</option>
+        <option value="km">Kilometros</option>
+        <option value="um">Micrometros</option>
+        <option value="mi">Millas</option>
+        <option value="mm">Milimetros</option>
+        <option value="nm">Nanometros</option>
+        <option value="pc">Picometros</option>
+        <option value="yd">Yardas</option>
     </select>
     
-    <input type="submit" value="Convertir">
-</form>
+    <input type="submit" class="mt-3" value="Convertir">
+    </form>
 
     </div>
 
