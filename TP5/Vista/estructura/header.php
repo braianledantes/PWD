c647b9c204ae1b0900711bdd85c4ec4298568609
Braian Ledantes <braianledantes@gmail.com>
2024-10-31 20:42:56 -0300

    Actualiza enlaces de navegación para reflejar la transición a TP5

--- TP5/Vista/estructura/header.php
+++ TP5/Vista/estructura/header.php
@@ -1,35 +1,17 @@
 <header class="position-sticky top-0 shadow mb-3">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="../../index.php">PWD TP4</a>
+            <a class="navbar-brand" href="../../index.php">PWD</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/index.php">Inicio</a>
+                        <a class="nav-link" href="/pwd/TP5/Vista/listarUsuario.php">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/VerAutos.php">Ver Autos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/buscarAuto.php">Buscar Auto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/listaPersonas.php">Listar Personas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/NuevaPersona.php">Nueva Persona</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/NuevoAuto.php">Nuevo Auto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/CambioDuenio.php">Cambio Dueño</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PWD/TP4/Vistas/BuscarPersona.php">Buscar Persona</a>
+                        <a class="nav-link" href="/pwdd/TP5/Vista/login.php">Login</a>
                     </li>
                 </ul>
                 <img class="ms-auto me-3" style="height: 35px; width: auto;" src="https://sipcosystems.com/images/web-application-development.png" alt="PWD">
