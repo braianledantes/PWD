44b1d634f405ce9ce23a9d9637110eb0bcff8c3c
Braian Ledantes <braianledantes@gmail.com>
2024-11-08 19:17:56 -0300

    ejemplo de sesion de malapi

---
+++ PWD_Seguridad/configuracion.php
@@ -0,0 +1,8 @@
+<?php
+$PROYECTO ='PWD/PWD_Seguridad';
+
+//variable que almacena el directorio del proyecto
+$ROOT =$_SERVER['DOCUMENT_ROOT']."/$PROYECTO/";
+
+include_once($ROOT.'utiles/funciones.php');
+?>
